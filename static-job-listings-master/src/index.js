2aab2d305b30152480a3895571d13d79d80bc79f
NehuenCuenca <nehuen.cuenca@hotmail.com>
2022-02-03 19:05:21 -0300

    worst filter job of the history

--- static-job-listings-master/src/index.js
+++ static-job-listings-master/src/index.js
@@ -1,8 +1,13 @@
-const jobsList= document.querySelector('.jobList')
+const jobsList = document.querySelector('.jobList')
+const filter = document.querySelector('.tagsColumn')
+const btnClearTags = document.querySelector('#btnClearTags')
+
 let jobs= []
+let jobTags
 
 let filtersTag = []
 let allTags= []
+let tagsByJob= []
 
 function getData() {
     fetch('../data.json')
@@ -18,9 +23,11 @@ function getData() {
 getData()
 setTimeout(() => {
     console.log(jobs);
-}, 1000 );
+}, 500 );
 
 function printJobs(arrayJobs) {
+    jobsList.innerHTML=``;
+    
     arrayJobs.forEach((job, i) => {
         let isNew= job.new ? `<span class="tagInfo new">New!</span>` : ''
         let isFeatured= job.featured ? `<span class="tagInfo featured">Featured</span>` : ''
@@ -30,8 +37,9 @@ function printJobs(arrayJobs) {
         jobTags.push(job.role, job.level, ...job.languages, ...job.tools)
 
         jobTags.forEach((tag) => {
-            tags += `<button class="btnTagJob">${tag}</button>`
+            tags += `<div class="btnTagJob" data-value="${tag}">${tag}</div>`
         });
+        tagsByJob.push([...jobTags])
 
 
         jobsList.innerHTML += `<li>
@@ -59,16 +67,21 @@ function printJobs(arrayJobs) {
                                         ${tags}
                                     </div>
                                 </li>`;
+
         allTags.push(...jobTags)
     });
-    
+
+    recordBTNtags();
     allTags= removeDuplicates(allTags)
-    console.log(allTags); 
 }
 
 setTimeout(() => {
     printJobs(jobs)
-}, 1000);
+    console.log(tagsByJob);
+    console.log(allTags); 
+    /* filterListByTags() */
+}, 500);
+
 
 function removeDuplicates(array) {
     const dataArr = new Set(array);
@@ -76,3 +89,120 @@ function removeDuplicates(array) {
 
     return result
 }
+
+
+function filterListByTags(arrTags=[]) {
+    let indexesJob = []
+
+    arrTags.forEach((tagF, i) => {
+        tagsByJob.forEach((tags, j) => {
+            tags.forEach((tag) => {
+                if(tag == arrTags[i]){
+                    indexesJob.push(j)
+                }   
+            })  
+        })
+    })
+
+    indexesJob=ordenarArray(removeDuplicates(indexesJob))
+
+    let filteredJobs = []
+    for (let i = 0; i < indexesJob.length; i++) {
+        for (let j = 0; j < jobs.length; j++) {
+            if(indexesJob[i] == j){
+                filteredJobs.push(jobs[indexesJob[i]]);
+            }
+        }
+    }
+
+    /* arrTags.length==0 ? filteredJobs= [...allTags] : filteredJobs= ['Vue'] */
+    console.log(arrTags);
+    console.log(filteredJobs);
+    return filteredJobs
+}
+
+//orden de menor a mayor
+function ordenarArray(array) {
+    return array.sort((a,b)=>a-b)
+}
+
+function recordBTNtags() {
+    const btnTags = document.querySelectorAll('div.jobTags div')
+    const btnDeleteTag = document.querySelectorAll('div.btnTagJob span')
+    console.log(btnDeleteTag);
+    console.log(filter.childNodes);
+    
+
+    //X click
+    btnDeleteTag.forEach(btn => {
+        btn.addEventListener('click', () =>{
+            setTimeout(() => {
+                filter.removeChild(btn.parentElement);
+            }, 500);
+            
+            setTimeout(() => {
+                let filters = getFiltersNow()
+                console.log(filters);
+                printJobs(filterListByTags(filters))
+            }, 500);
+        })
+    }); 
+    
+    //tag click
+    btnTags.forEach(btn => {
+        btn.addEventListener('click', () =>{
+            if(findTagRepeated(btn.textContent)){
+                return
+            } else {
+                filter.innerHTML += `<div class="btnTagJob" data-value="${btn.textContent}">${btn.textContent}<span>X</span></div>`;
+                
+                setTimeout(() => {
+                    let filters = getFiltersNow()
+                    printJobs(filterListByTags(filters))
+                }, 500);
+            }
+        })
+    }); 
+
+}
+
+
+function findTagRepeated(tag){
+    let finded = false;
+    filter.childNodes.forEach((tagFilter) => {
+        if(tagFilter.dataset.value == tag){
+            finded= true
+        }
+    })
+    
+    return finded
+}
+
+function getFiltersNow() {
+    let tagsBTNS= filter.childNodes;
+    let tagsNow= []
+    
+    console.log(tagsBTNS.length);
+    if(tagsBTNS.length==0){
+        return tagsNow= [...allTags]
+    }
+
+    tagsBTNS.forEach((tag, i) => {
+        tagsNow.push(tag.dataset.value);
+    });
+
+    return tagsNow;
+}
+
+
+btnClearTags.addEventListener('click', () => {
+    setTimeout(() => {
+        filter.innerHTML= ``;
+    }, 200);
+    
+    setTimeout(() => {
+        let filters = getFiltersNow()
+        console.log(filters);
+        printJobs(filterListByTags(filters))
+    }, 500);
+})
